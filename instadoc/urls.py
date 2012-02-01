6b74c2f307463c878b53c7cef921f7f45e9fb2b6
Chris <chris@imendo.at>
2012-02-01 10:28:14 +0100

    url mapping

--- instadoc/urls.py
+++ instadoc/urls.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.contrib import admin
 
@@ -11,14 +10,8 @@ from main.api import v1_api
 
 
 urlpatterns = patterns('',
-  # main redirect
-  url(r'^$', RedirectView.as_view(url="%s/main/" % settings.APP_ROOT_URL)),
-
-  # main app
-  url(r'^main/', include('%s.main.urls' % settings.PROJECT_NAME, namespace="main")),
   url(r'^api/', include(v1_api.urls)),
-
-
+  url(r'^', include('%s.main.urls' % settings.PROJECT_NAME, namespace="main")),
 )
 
 if "django.contrib.admin" in settings.INSTALLED_APPS:
