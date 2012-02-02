b4957f1bc07065327fc0cc8566f8ced3e86cfab4
Chris <chris@ca-net.org>
2012-02-02 11:10:08 +0100

    disqus comments support

--- instadoc/main/context_processors.py
+++ instadoc/main/context_processors.py
@@ -1,9 +1,11 @@
 
 from django.conf import settings
 
+
 def django_conf(request):
   return dict(
     APP_NAME=settings.APP_NAME,
-    APP_VERSION=settings.APP_VERSION
+    APP_VERSION=settings.APP_VERSION,
+    DEBUG=settings.DEBUG
   )
 
